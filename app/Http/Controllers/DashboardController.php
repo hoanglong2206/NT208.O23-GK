6d4bfe36801af7e76b2abb813e89930796bd1fda
hoanglong2206 <21522307@gm.uit.edu.vn>
2024-03-13 11:50:25 +0700

    Completed CRUD

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -9,8 +9,14 @@ class DashboardController extends Controller
 {
     public function index()
     {
+        $ideas = Idea::orderBy('created_at', 'desc');
+
+        if (request()->has('search')) {
+            $ideas = $ideas->where('content', 'like', '%' . request()->get('search', '') . '%');
+        }
+
         return view('dashboard', [
-            'ideas' => Idea::orderBy('created_at', 'desc')->paginate(3),
+            'ideas' => $ideas->paginate(3),
         ]);
     }
 }
--- resources/views/shared/idea-card.blade.php
+++ resources/views/shared/idea-card.blade.php
@@ -9,9 +9,35 @@
                         </a></h5>
                 </div>
             </div>
+            <div>
+                <form action="{{ route('ideas.destroy', $idea->id) }}" method="post">
+                    @csrf
+                    @method('DELETE')
+                    <a class="mx-2" href="{{ route('ideas.edit', $idea->id) }}">Edit</a>
+                    <a href="{{ route('ideas.show', $idea->id) }}">View</a>
+                    <button class="btn btn-danger btn-sm">X</button>
+                </form>
+            </div>
         </div>
     </div>
     <div class="card-body">
+        @if ($editing ?? false)
+            <form action="{{ route('ideas.update', $idea->id) }}" method="post">
+                @csrf
+                @method('PUT')
+                <div class="mb-3">
+                    <textarea name="content" class="form-control" id="content" rows="3">{{ $idea->content }}</textarea>
+                    @error('content')
+                        <span class="d-block fs-6 text-danger mt-2">
+                            {{ $message }}
+                        </span>
+                    @enderror
+                </div>
+                <div class="">
+                    <button class="btn btn-dark mt-2 btn-small"> Submit </button>
+                </div>
+            </form>
+        @endif
         <p class="fs-6 fw-light text-muted">
             {{ $idea->content }}
         </p>
